ab78cea4183988cb44f15e36f90cd0d3ba8105bc
BLACK PHOENIX <144792385+sudarshanjundre@users.noreply.github.com>
2023-10-22 15:17:34 +0530

    Create Day 2.c

---
+++ Day 2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter first number:-");
+    scanf("%d",&a);
+    printf("Enter second number:-");
+    scanf("%d",&b);
+  printf("Swapped numbers:-%d\n\t\t%d",b,a);
+    return 0;
+}
