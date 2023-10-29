f7e76d8ad49ac443d90de079839dbf0196c9946e
BLACK PHOENIX <144792385+sudarshanjundre@users.noreply.github.com>
2023-10-29 06:40:58 +0530

    Create Day 2.py

---
+++ Day 2.py
@@ -0,0 +1,10 @@
+#Program for swapping of two numbers without using third variable.
+a=int(input("Enter first number:-"))
+b=int(input("Enter second number:-"))
+a=a+b
+b=a-b
+a=a-b
+print("Swapped numbers:-")
+print(a)
+print(b)
+
